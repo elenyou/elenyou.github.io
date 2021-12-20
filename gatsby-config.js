fe4de85d800aa511de8d5337f11e7644d10bc31f
Lena <olenaovch@gmail.com>
2021-12-20 11:11:50 -0500

    new resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
     siteMetadata: {
-      description: "Frontend Developer from London, Ontario, Canada. React Native apps, Next.js, Custom WordPress themes.",
+      description: "Frontend Developer from London, Ontario, Canada.",
       locale: "en",
-      title: "Sergey Ovcharenko. Frontend Developer",
+      title: "Lena Ovcharenko. Frontend Developer",
+      formspreeEndpoint:"https://formspree.io/f/xknynavy",
     },
     plugins: [
       {
@@ -11,7 +12,19 @@ module.exports = {
           basePath: "/",
           contentPath: "content/",
           showThemeLogo: false,
-          theme: "dark-pink",
+          theme: {
+            'back': '#161b23',
+            'front': '#9dadc5',
+            'lead': '#91e12a',
+            'lead-text': '#161b23',
+            'line': '#28303c',
+            'skill-1': '#25a55f',
+            'skill-2': '#9bdf46',
+            'skill-3': '#e9f679',
+            'skill-4': '#e9f679',
+            'skill-5': '#9bdf46',
+            'skill-6': '#25a55f',
+          },
         },
       },
     ],
